a1eca56506914c53aaab2d119536724afb94b78c
Morten Kolstad <morten.kolstad@bekk.no>
2022-10-16 12:42:43 +0200

    List done

--- haskell/app/List.hs
+++ haskell/app/List.hs
@@ -10,18 +10,25 @@ import TestUtil ( quickcheckWithLabel )
 -- #Rev 1
 --skriv en funksjon som reverserer en liste ved å ++ elementer bakerst
 reverse :: [a] -> [a]
-reverse = error "TODO reverse"
+reverse [] = []
+reverse (x:xs) = reverse xs ++ [x]
 
 -- #Rev 2
 -- Hva er suboptimalt med løsningen over?
 -- Hva er kjøretidskompleksiteten?
 -- Hva burde vi forvente den er?
+q =     "append er lineær i første (this) argumentet, O(n)." <>
+    "siden man gjør dette for hver element i listen man reverserer, så blir det kvadratisk O(n^2)." <>
+    "Man burde kunne forvente at den er O(n)"
 
 -- #Rev 3
 -- Skriv reverse på nytt, men en mer optimal versjon
 -- Hint : lag enda en funksjon (gjerne internt i reverse')
 reverse' :: [a] -> [a]
-reverse' = error "TODO reverse"
+reverse' xs = helper xs []
+    where
+        helper [] res = res
+        helper (y:ys) res = helper ys (y:res)
 
 ---------
 
